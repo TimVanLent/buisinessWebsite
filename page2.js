7c1b12f292186c18f25cceb162e5b0395e7141df
Kevin <kevindejonge1991@gmail.com>
2016-01-22 13:48:36 +0100

    new page2

--- page2.js
+++ page2.js
@@ -1,1 +1,18 @@
-indexwebsite.js
+
+
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect: 'none',
+		closeEffect: 'none'
+	});
+});
+
+$('nav li ul').hide().removeClass('fallback');
+$('nav li').hover(
+	function() {
+		$('ul', this).stop().slideDown(200);
+	},
+	function() {
+		$('ul', this).stop().slideUp(200);
+	}
+);
